2c48739687f32608ff00bf694e17551226e06977
plfj <222489624+plfj@users.noreply.github.com>
2026-03-30 14:05:40 +0600

    Fixed

--- build.sh
+++ build.sh
@@ -1312,8 +1312,7 @@ _post_install() {
                "${TERMUX_PREFIX}/share/man/man1/python.1" 2>/dev/null || true
     fi
 
-    rm -rf $TERMUX_PREFIX/lib/__pycache__
-    rm -rf $TERMUX_PREFIX/lib/*/__pycache__
+    find $TERMUX_PREFIX/lib/python3.13 -type d -name "__pycache__" -exec rm -rf {} +
     local debpython_src="${TERMUX_PKG_SRCDIR}/debpython/debpython"
     if [[ -d "$debpython_src" ]]; then
         local debpython_dst="${TERMUX_PREFIX}/lib/python${_MAJOR_VERSION}/debpython"
